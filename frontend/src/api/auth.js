1b35e92c0775479aa311057af4c885674163e1e7
Dinithi14 <dinithidulani18@gmail.com>
2026-08-31 13:31:00 +0530

    Update users list UI

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -8,6 +8,7 @@ export const loginUser = async (email, password) => {
 export const registerUser = async (data) => {
   const response = await api.post('/auth/register', data);
   return response.data;
+  
 };
 
 export const verifyEmail = async (data) => {
